cedd3bca29b17c6c283a65ed377d54bbbcdd6b24
Jonas Petersson <48443256+jonasPwnas@users.noreply.github.com>
2026-06-25 09:40:36 +0200

    Revert "Operation salvage has commenced"
    This reverts commit 3b66442efdbd4c760732285be81c3fc72e5a044f.

--- Source/SnakeyCpp/Private/SnakePlayer.cpp
+++ Source/SnakeyCpp/Private/SnakePlayer.cpp
@@ -4,309 +4,33 @@
 #include "SnakePlayer.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include "SnakePlayerState.h"
 
 // Sets default values
-ASnakePawn::ASnakePawn()
+ASnakePlayer::ASnakePlayer()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-TArray<FIntPoint> ASnakePawn::GetAllOccupiedGridCells() const
-{
-	TArray<FIntPoint> Occupied = BodyGridPositions;
-	Occupied.Insert(CurrentGridPosition, 0);
-	return Occupied;
-}
-
 // Called when the game starts or when spawned
-void ASnakePawn::BeginPlay()
+void ASnakePlayer::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 // Called every frame
-void ASnakePawn::Tick(float DeltaTime)
+void ASnakePlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
 // Called to bind functionality to input
-void ASnakePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ASnakePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	
-	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
-	{
-		if (ForwardAction)
-		{
-			EnhancedInputComponent->BindAction(ForwardAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnForward);
-		}
-		if (BackwardAction)
-		{
-			EnhancedInputComponent->BindAction(BackwardAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnBackward);
-		}
-		
-		if (UpAction)
-		{
-			EnhancedInputComponent->BindAction(UpAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnUp);
-		}
-		if (DownAction)
-		{
-			EnhancedInputComponent->BindAction(DownAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnDown);
-		}
 
-		if (LeftAction)
-		{
-			EnhancedInputComponent->BindAction(LeftAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnLeft);
-		}
-		if (RightAction)
-		{
-			EnhancedInputComponent->BindAction(RightAction, ETriggerEvent::Triggered, this, &ASnakePawn::Input_TryTurnRight);
-		}
-	}
-}
-
-void ASnakePawn::PossessedBy(AController* NewController)
-{
-	Super::PossessedBy(NewController);
-	SetupEnhancedInput();
-}
-
-void ASnakePawn::PawnClientRestart()
-{
-	Super::PawnClientRestart();
-	SetupEnhancedInput();
-}
-
-void ASnakePawn::SetupEnhancedInput()
-{
-	if (APlayerController* PC = Cast<APlayerController>(GetController()))
-	{
-		if (ULocalPlayer* LocalPlayer = PC->GetLocalPlayer())
-		{
-			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
-			{
-				if (SnakeInputMapping)
-				{
-					Subsystem->AddMappingContext(SnakeInputMapping, 0); // We add the Input Mapping Context to the player's Enhanced Input Subsystem, with a priority of 0 (higher priority contexts will override lower priority ones if they have overlapping bindings)
-				}
-			}
-		}
-	}
-}
-
-FVector ASnakePawn::GetVectorFromDirection(ESnakeDirection Direction) const
-{
-	switch (Direction)
-	{
-		case ESnakeDirection::Forward: return FVector::ForwardVector;
-		case ESnakeDirection::Backward: return FVector::BackwardVector;
-		case ESnakeDirection::Right: return FVector::RightVector;
-		case ESnakeDirection::Left: return FVector::LeftVector;
-		case ESnakeDirection::Up: return FVector::UpVector;
-		case ESnakeDirection::Down: return FVector::DownVector;
-		default: return FVector::ZeroVector;
-	}
-}
-
-FIntPoint ASnakePawn::DirectionToGridOffset(ESnakeDirection Direction) const
-{
-	switch (Direction)
-	{
-	case ESnakeDirection::Forward: return FIntPoint(1, 0);
-	case ESnakeDirection::Backward: return FIntPoint(-1, 0);
-	case ESnakeDirection::Left: return FIntPoint(0, -1);
-	case ESnakeDirection::Right: return FIntPoint(0, 1);
-		/*How should I handle height? Probably a seperate value for gridlike gravity that allows for jumping
-	*case ESnakeDirection::Up: return FIntPoint(1, 0);
-	*case ESnakeDirection::Down: return FIntPoint(-1, 0); */
-	default: return FIntPoint(0, 0);
-	}
-}
-
-FVector ASnakePawn::GridToWorldLocation(const FIntPoint& GridPosition) const
-{
-	return FVector::ZeroVector;
-}
-
-//Input
-void ASnakePawn::Input_TryTurnForward(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Forward;
-	}
-}
-void ASnakePawn::Input_TryTurnBackward(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Backward;
-	}
-}
-
-void ASnakePawn::Input_TryTurnUp(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Up;
-	}
-}
-void ASnakePawn::Input_TryTurnDown(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Down;
-	}
-}
-
-void ASnakePawn::Input_TryTurnLeft(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Left;
-	}
-}
-void ASnakePawn::Input_TryTurnRight(const FInputActionValue& Value)
-{
-	bool bPressed = Value.Get<bool>();
-	if (bPressed)
-	{
-		RequestedDirection = ESnakeDirection::Right;
-	}
-}
-
-//Movement
-void ASnakePawn::HandleDirectionChange()
-{
-	if (CurrentDirection != RequestedDirection && IsValidTurn(RequestedDirection))
-	{
-		CurrentDirection = RequestedDirection;
-		UpdateDirection(CurrentDirection);
-		UE_LOG(LogTemp, Type::Display, TEXT("Direction changed to: %s"), *UEnum::GetValueAsString(CurrentDirection));
-	}
-}
-
-void ASnakePawn::UpdateDirection(ESnakeDirection NewDirection)
-{
-	switch (NewDirection)
-	{
-	case ESnakeDirection::Forward:	SetActorRotation(FRotator(0.f, 0.f, 0.f));
-		break;
-	case ESnakeDirection::Backward:	SetActorRotation(FRotator(0.f, 180.f, 0.f));
-		break;
-	case ESnakeDirection::Left:	SetActorRotation(FRotator(0.f, -90.f, 0.f));
-		break;
-	case ESnakeDirection::Right:SetActorRotation(FRotator(0.f, 90.f, 0.f));
-		break;
-		/* again whaaat to do. hmm
-	case ESnakeDirection::Up:	SetActorRotation(FRotator(0.f, 0.f, 0.f));
-		break;
-	case ESnakeDirection::Down:	SetActorRotation(FRotator(0.f, 180.f, 0.f));
-		break;
-		*/
-	}
-}
-
-void ASnakePawn::TickGridMovement(float DeltaTime)
-{
-	if (MoveStepTime <= 0.f)
-	{
-		return;
-	}
-
-	if (!bIsMovingToTarget)
-	{
-		// If at target, check for direction change and update target location
-		StartNewMoveStep();
-		if (!bIsMovingToTarget) //early out if not moving to target
-		{
-			return; 
-		}
-	}
-
-	MoveInterpolationProgress += DeltaTime / MoveStepTime;
-	const float Alpha = FMath::Clamp(MoveInterpolationProgress, 0.f, 1.f);
-
-	// Use Lerp for constant speed across the cell
-	const FVector NewHeadLocation = FMath::Lerp(StepStartWorldLocation, StepTargetWorldLocation, Alpha);
-	SetActorLocation(NewHeadLocation, false);
-
-	UpdateBodyVisuals(Alpha); 
-	
-	if (Alpha >= 1.f)
-	{
-		// We've reached the target grid location
-		FinishMoveStep();
-	}
-}
-
-void ASnakePawn::TickFreeMovement(float DeltaTime)
-{
-	FVector MovementVector = GetVectorFromDirection(CurrentDirection);
-	FVector DesiredOffset = MovementVector * MoveSpeed * DeltaTime;
-	AddActorWorldOffset(DesiredOffset, true);
-}
-
-bool ASnakePawn::IsValidTurn(ESnakeDirection NewDirection) const
-{
-	return false;
-}
-
-void ASnakePawn::DrawDebugInfo()
-{
-}
-
-FIntPoint ASnakePawn::GetClampedStartGridPosition() const
-{
-	return FIntPoint(0, 0);
-}
-
-bool ASnakePawn::WouldHitWall(const FIntPoint& NextCell) const
-{
-	return false;
-}
-
-bool ASnakePawn::WouldHitSelf(const FIntPoint& NextCell) const
-{
-	return false;
-}
-
-//Snek Body
-void ASnakePawn::StartNewMoveStep()
-{
-}
-
-void ASnakePawn::FinishMoveStep()
-{
-}
-
-void ASnakePawn::UpdateBodyVisuals(float Alpha)
-{
-}
-
-void ASnakePawn::EnsureBodySegmentMeshCount()
-{
-}
-
-void ASnakePawn::ClearBodyVisuals()
-{
-}
-
-void ASnakePawn::GrowSnake(int32 Amount)
-{
-}
-
-void ASnakePawn::AddInitialBodySegments(int32 NumSegments)
-{
 }
 
--- Source/SnakeyCpp/Public/SnakePlayer.h
+++ Source/SnakeyCpp/Public/SnakePlayer.h
@@ -3,178 +3,49 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "InputActionValue.h"
-#include "SnakeGrid.h"
 #include "GameFramework/Pawn.h"
 #include "SnakePlayer.generated.h"
 
+class UInputAction;
 class UCameraComponent;
 class UInputMappingContext;
-class UInputAction;
 class UStaticMeshComponent;
 class USpringArmComponent;
 class USphereComponent;
 
-
-UENUM(Blueprintable)
-enum class ESnakeDirection : uint8
-{
-	Forward, Backward, Left, Right, Up, Down
-};
-
 UCLASS()
-class SNAKEYCPP_API ASnakePawn : public APawn
+class SNAKEYCPP_API ASnakePlayer : public APawn
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this pawn's properties
-	ASnakePawn();
-	
-	UFUNCTION(BlueprintPure, Category = "Snake")
-	TArray<FIntPoint> GetAllOccupiedGridCells() const;
-	
-protected:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	virtual void PossessedBy(AController* NewController) override;
-	virtual void PawnClientRestart() override;
-	void SetupEnhancedInput();
+	ASnakePlayer();
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (ClampMin = "0.01", ClampMax = "1.0"))
-	float MoveStepTime = 0.2f; // Time it takes to move from one grid cell to the next when using grid movement.
-	
-private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Snake", meta = (AllowPrivateAccess = true))
-	TArray<FIntPoint> BodyGridPositions;
-	
-	UPROPERTY()
-	TObjectPtr<ASnakeGrid> SnakeGrid;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Snake", meta = (AllowPrivateAccess = true))
-	int32 PendingGrowth = 0;
-	
-	ESnakeDirection CurrentDirection = ESnakeDirection::Forward;
-	ESnakeDirection RequestedDirection = ESnakeDirection::Forward;
-	FIntPoint CurrentGridPosition = FIntPoint(0, 0);
-	FIntPoint PendingNextGridPosition = FIntPoint(0, 0);
-	
-	FVector StepStartWorldLocation = FVector::ZeroVector;
-	FVector StepTargetWorldLocation = FVector::ZeroVector;
-	
-	float MoveInterpolationProgress = 0.f;
-	bool bIsMovingToTarget = false;
-	bool bIsDead = false;
-	FTimerHandle ResetTimerHandle;
-	
-	TArray<FIntPoint> PreviousBodyGridPositions;
-	TArray<FIntPoint> TargetBodyGridPositions;
-	
-	UPROPERTY(Transient)
-	TArray<TObjectPtr<UStaticMeshComponent>> BodyMeshes;
-	
-	//Helpers
-	FVector GetVectorFromDirection(ESnakeDirection Direction) const;
-	FIntPoint DirectionToGridOffset(ESnakeDirection Direction) const;
-	FVector GridToWorldLocation(const FIntPoint& GridPosition) const; //Move this to grid actor
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UEnhancedInputComponent> SnakeInputComp;
 
-	// Input callback functions
-	void Input_TryTurnForward(const FInputActionValue& Value);
-	void Input_TryTurnBackward(const FInputActionValue& Value);
-	void Input_TryTurnUp(const FInputActionValue& Value);
-	void Input_TryTurnDown(const FInputActionValue& Value);
-	void Input_TryTurnLeft(const FInputActionValue& Value);
-	void Input_TryTurnRight(const FInputActionValue& Value);
-	
-	//Movement
-	void HandleDirectionChange();
-	void UpdateDirection(ESnakeDirection NewDirection);
-	void TickGridMovement(float DeltaTime);
-	void TickFreeMovement(float DeltaTime);
-	bool IsValidTurn(ESnakeDirection NewDirection) const;
-	void DrawDebugInfo();
-	FIntPoint GetClampedStartGridPosition() const;
-	bool WouldHitWall(const FIntPoint& NextCell) const;
-	bool WouldHitSelf(const FIntPoint& NextCell) const;
-
-	// Body system
-	void StartNewMoveStep();
-	void FinishMoveStep();
-	void UpdateBodyVisuals(float Alpha);			
-	void EnsureBodySegmentMeshCount();
-	void ClearBodyVisuals();
-	void GrowSnake(int32 Amount = 1);
-	void AddInitialBodySegments(int32 NumSegments);
-	
-	// Components
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UStaticMeshComponent> HeadMesh;
-		
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<USphereComponent> CollisionSphere;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<USpringArmComponent> SpringArm;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UCameraComponent> Camera;
-	
-	//Input
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UInputMappingContext> SnakeInputMapping;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UInputAction> ForwardAction;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UInputAction> BackwardAction;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> UpAction;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> DownAction;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> LeftAction;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> RightAction;
 	
-	// Movement / Grid
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	bool bUseGridMovement = true;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Grid", meta = (AllowPrivateAccess = "true"))
-	FIntPoint StartGridPosition = FIntPoint(10, 10);
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Grid", meta = (AllowPrivateAccess = "true"))
-	FVector GridOrigin = FVector::ZeroVector;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float MoveSpeed = 100.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug", meta = (AllowPrivateAccess = "true", ClampMin = "1.0", ClampMax = "100.0"))
-	float CollisionSphereRadius = 32.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug", meta = (AllowPrivateAccess = "true"))
-	bool bShowDebugCollision = true;
-
-	// Body visuals settings
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Snake|Body", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UStaticMesh> BodySegmentMeshAsset = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Snake|Body", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UMaterialInterface> BodySegmentMaterial = nullptr;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Snake", meta = (AllowPrivateAccess = "true"))
-	int32 InitialBodyLength = 3;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Snake|Body", meta = (AllowPrivateAccess = "true"))
-	FVector BodySegmentMeshScale = FVector(0.9f, 0.9f, 0.9f);
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Snake|Body", meta = (AllowPrivateAccess = "true"))
-	float BodySegmentMeshZOffset = 0.01f;
-	
 };
